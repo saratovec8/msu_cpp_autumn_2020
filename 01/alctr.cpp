b4e67be56ea643ed95ebbc141d0d752ea79230ea
astrawoke <adr.konovalov@gmail.com>
2020-10-14 23:24:21 +0300

    My 4 commit

--- 01/alctr.cpp
+++ 01/alctr.cpp
@@ -1,59 +1,70 @@
 #include<iostream>
-class Allocator{
+class Allocator
+{
 	private:
 		char *start_mem;
 		size_t current;
 		size_t msize;
 	public:
 		Allocator()
-		{
-		start_mem = nullptr;
-		current = 0;
-		msize = 0;
-		}
+            {
+                start_mem = nullptr;
+                current = 0;
+                msize = 0;
+            }
 		~Allocator()
-		{
-			std::free(start_mem);
-		}
+            {
+                std::free(start_mem);
+            }
 		void makeAllocator(size_t maxSize)
-		{
-			
-			if(start_mem != nullptr){
-			start_mem = static_cast<char*>(std::realloc(start_mem,maxSize));
-			}else{
-			start_mem = static_cast<char*>(std::malloc(maxSize* sizeof(char)));
-			}
+            {
+                if(start_mem != nullptr)
+                {
+                    start_mem = static_cast<char*>(std::realloc(start_mem,maxSize));
+                }
+                else
+                {
+                    start_mem = static_cast<char*>(std::malloc(maxSize* sizeof(char)));
+                }
 
-			msize = maxSize;
-			current = 0;
-		}
+                msize = maxSize;
+                current = 0;
+            }
+
 		char* alloc(size_t size)
-		{
-			if(current + size > msize){
-			return nullptr;
-			} else {
-			current += size;
-			return start_mem + current - size;
-			}
-			
-		}
+            {
+                if(current + size > msize)
+                {
+                    return nullptr;
+                }
+                else
+                {
+                    current += size;
+                    return start_mem + current - size;
+                }
+
+            }
+
 		void reset()
-		{
-			current = 0;
-		}
+            {
+                current = 0;
+            }
 
-		size_t ret_current(){
-		return current;
-		}
+		size_t ret_current()
+            {
+                return current;
+            }
 
-		size_t ret_msize(){
-		return msize;
-		}
+		size_t ret_msize()
+            {
+                return msize;
+            }
 
-		char* ret_start_mem(){
-		return start_mem;
-		}
-                
+		char* ret_start_mem()
+            {
+                return start_mem;
+            }
+
 
 };
 
--- 01/test.cpp
+++ 01/test.cpp
@@ -1,74 +1,76 @@
 #include<iostream>
 #include<cassert>
 #include"alctr.cpp"
+
 void DefaultWorkTest()
-{
-	Allocator A;
-	A.makeAllocator(100);
-	char* result_0 = A.alloc(0);
-	char* result_1= A.alloc(50);
-	char* result_2 = A.alloc(20);
-	char* result_3 = A.alloc(0);
-	assert(result_3-result_0 == 70); 
-}
+    {
+        Allocator A;
+        A.makeAllocator(100);
+        char* result_0 = A.alloc(0);
+        char* result_1= A.alloc(50);
+        char* result_2 = A.alloc(20);
+        char* result_3 = A.alloc(0);
+        assert(result_3-result_0 == 70);
+    }
 
 void DefaultCreateTest()
-{
-	Allocator A;
-	A.makeAllocator(20);
-	assert(A.ret_start_mem() != nullptr && A.ret_current() == 0 && A.ret_msize() == 20 );
+    {
+        Allocator A;
+        A.makeAllocator(20);
+        assert(A.ret_start_mem() != nullptr && A.ret_current() == 0 && A.ret_msize() == 20 );
 
-}
+    }
 
 void CheckResetTest()
-{
-	Allocator A;
-	A.makeAllocator(200);
-	char* result_0 = A.alloc(0);
-	char* result_1 = A.alloc(60);
-	char* result_2 = A.alloc(70);
-	A.reset();
-	assert(A.alloc(0) == result_0);
-}
+    {
+        Allocator A;
+        A.makeAllocator(200);
+        char* result_0 = A.alloc(0);
+        char* result_1 = A.alloc(60);
+        char* result_2 = A.alloc(70);
+        A.reset();
+        assert(A.alloc(0) == result_0);
+    }
 
 void OutOfBufferTest()
-{
-	Allocator A;
-	A.makeAllocator(50);
-	char* result = A.alloc(54);
-	assert(result == nullptr);
-}
+    {
+        Allocator A;
+        A.makeAllocator(50);
+        char* result = A.alloc(54);
+        assert(result == nullptr);
+    }
 
 void UsingMemAfterReset()
-{
-	Allocator A;
-	A.makeAllocator(20);
-	char* result_1 = A.alloc(10);
-	A.reset();
-	char* result_2 = A.alloc(10);
-	//char* result_3 = A.alloc(0);
-	assert(result_1 == result_2);
-}
+    {
+        Allocator A;
+        A.makeAllocator(20);
+        char* result_1 = A.alloc(10);
+        A.reset();
+        char* result_2 = A.alloc(10);
+        //char* result_3 = A.alloc(0);
+        assert(result_1 == result_2);
+    }
 
 void FullMemFill()
-{
-	Allocator A;
-	A.makeAllocator(51);
-	char* result_0 =  A.alloc(0);
-	char* result_1 = A.alloc(30);
-	char* result_2 =  A.alloc(20);
-	char* result_3 = A.alloc(0);
-	assert(result_3 == result_0 + 50);
-}
-int main(){
-	DefaultWorkTest();
-	CheckResetTest();
-	OutOfBufferTest();
-	UsingMemAfterReset();
-	FullMemFill();
-	DefaultCreateTest();
+    {
+        Allocator A;
+        A.makeAllocator(51);
+        char* result_0 =  A.alloc(0);
+        char* result_1 = A.alloc(30);
+        char* result_2 =  A.alloc(20);
+        char* result_3 = A.alloc(0);
+        assert(result_3 == result_0 + 50);
+    }
 
-	std::cout << "success \n";
+int main()
+    {
+        DefaultWorkTest();
+        CheckResetTest();
+        OutOfBufferTest();
+        UsingMemAfterReset();
+        FullMemFill();
+        DefaultCreateTest();
 
-	return 0;
-}
+        std::cout << "success \n";
+        return 0;
+    }
