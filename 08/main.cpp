588856a9fa832fe7c90008c814867f7c5b42ec86
astrawoke <adr.konovalov@gmail.com>
2020-12-21 21:16:51 +0300

    My 26 commit

---
+++ 08/main.cpp
@@ -0,0 +1,49 @@
+#include "tpool.h"
+#include <numeric>
+#include <iostream>
+#include <unistd.h>
+#include <iostream>
+#include <cassert>
+#include <cmath>
+
+
+int64_t from_binary_to_decimal(const std::vector<int>& vec, size_t i, size_t j) 
+{
+	int res = 0;
+	for (size_t l = i; l < j; ++l) 
+	{
+		res += vec[l]*pow(2, l);;
+	}
+	return res;
+}
+
+
+int main()
+{
+	vector<int> vec(20, 1);
+	int64_t multiple_num = 0;
+	int64_t single_num = 0;
+
+	size_t thr_num = 4;
+	ThreadPool pool(thr_num);
+
+	vector<future<int64_t>> futures;
+
+	for (size_t i = 0; i != vec.size(); i += vec.size() / thr_num) 
+	{
+		futures.push_back(pool.exec(from_binary_to_decimal, vec, i, i + vec.size() / thr_num));
+	}
+
+	for (auto& f : futures) 
+	{
+		multiple_num += f.get();
+	}
+
+	single_num = from_binary_to_decimal(vec, 0, vec.size());
+
+	assert(multiple_num == single_num);
+
+	std::cout << "success" << std::endl;
+
+	return 0;
+}	
---
+++ 08/tpool.h
@@ -0,0 +1,75 @@
+#pragma once
+#include <condition_variable>
+#include <functional>
+#include <future>
+#include <mutex>
+#include <queue>
+#include <stdexcept>
+#include <thread>
+#include <vector>
+
+using namespace std;
+
+class ThreadPool 
+{
+	private:
+		std::vector<std::thread> _threads_;
+		std::queue<std::function<void()>> _queue_t_;
+		std::mutex _queue_m_;
+		std::condition_variable _mes_to_thr_;
+		std::atomic<bool> _stop_;
+
+	public:
+
+		template <class Func, class... Args>
+			auto exec(Func func, Args... args) -> std::future<decltype(func(args...))>
+			{
+				using type = decltype(func(args...));
+				auto task = std::make_shared<std::packaged_task<type()>>(std::packaged_task<type()>(std::move(std::bind(func, args...))));
+				std::future<type> res = task->get_future();
+				{
+					std::unique_lock<std::mutex> lock(_queue_m_);
+					if (_stop_)
+						throw std::runtime_error("thread terminated");
+
+					_queue_t_.push([task]() { (*task)(); });
+				}
+				_mes_to_thr_.notify_one();
+				return res;
+			}
+
+		explicit ThreadPool(size_t size) 
+		{
+			_stop_ = false;
+			for (size_t i = 0; i < size; ++i)
+				_threads_.emplace_back([this]() 
+						{
+						std::function<void()> task;
+						while (true) 
+						{
+						{
+						std::unique_lock<std::mutex> lock(_queue_m_);
+						_mes_to_thr_.wait(lock, [this] { return !_queue_t_.empty() || _stop_; });
+						if (_stop_ && _queue_t_.empty())
+						return;
+						task = std::move(_queue_t_.front());
+						_queue_t_.pop();
+						}
+						task();
+						}
+						});
+		}
+
+		~ThreadPool() 
+		{
+			_stop_ = true;
+			_mes_to_thr_.notify_all();
+			for (std::thread &thread : _threads_)
+				thread.join();
+		}
+
+
+		ThreadPool() = default;
+		ThreadPool& operator=(ThreadPool&) = default;
+
+};
